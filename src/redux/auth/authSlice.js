6e2e534b89ac47e3edd804bfc6c529fdacf19716
Vladyslav Simak <simakv21@gmail.com>
2024-04-15 13:18:55 +0300

    add toasty

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { register, logIn, logOut, refreshUser } from './operations';
+import { toast } from 'react-toastify';
 
 const initialState = {
   user: { name: null, email: null },
@@ -16,16 +17,50 @@ const authSlice = createSlice({
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
+
+      toast.success(`You are registered! Go to your email ${state.user.email} and get verified!`, {
+        position: "top-center",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     },
     [logIn.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
+
+      
+      toast.success(`You are logged in`, {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     },
     [logOut.fulfilled](state) {
       state.user = { name: null, email: null };
       state.token = null;
       state.isLoggedIn = false;
+
+      toast.error('You are logged out!', {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
     },
     [refreshUser.pending](state) {
       state.isRefreshing = true;
